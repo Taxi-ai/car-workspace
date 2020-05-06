767967665141244ef31c930e1131105d915f3336
AhmdNassar <engahmdnassar@gmail.com>
2020-05-06 07:07:08 +0200

    init main.cpp for trajectory planning

---
+++ planning/src/trajectory/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include "./map.hpp"
+
+using std::vector;
+
+
+int main()
+{
+    Map map;
+    // in current map data y -- > d and x -- > s where s && d is frenet corrdinat
+    double goalX, goalY; // this point should be get from mobil app (I think using web API)
+    // for test now
+    goalX = 0;
+    goalY = 100;
+
+    //TODO:
+    // * generat next 50 x,y points to move forward 
+}
