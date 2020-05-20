0ad28aa673f83ffdcf0e80966d5f7662ccc4f3ec
AhmdNassar <engahmdnassar@gmail.com>
2020-05-20 14:52:51 +0000

    Fix IMU pub erros && Add  imu msg to cmake

--- sensors/scripts/imu_pub.py
+++ sensors/scripts/imu_pub.py
@@ -6,6 +6,7 @@ import time
 import board
 import busio
 import adafruit_bno055
+import math  
 
 
 import serial
@@ -18,17 +19,19 @@ sensor = adafruit_bno055.BNO055(i2c)
 calibaration= False
 linearX=0
 
-xPos = 0, yPos = 0, headingVel = 0;
+xPos = 0
+yPos = 0
+headingVel = 0;
 BNO055_SAMPLERATE_DELAY_MS = 10; #how often to read data from the board
 
 #velocity = accel*dt (dt in seconds)
 #position = 0.5*accel*dt^2
-ACCEL_VEL_TRANSITION =  (double)(BNO055_SAMPLERATE_DELAY_MS) / 1000.0;
+ACCEL_VEL_TRANSITION =  (float)(BNO055_SAMPLERATE_DELAY_MS) / 1000.0;
 ACCEL_POS_TRANSITION = 0.5 * ACCEL_VEL_TRANSITION * ACCEL_VEL_TRANSITION;
-DEG_2_RAD = 0.01745329251; //trig functions require radians, BNO055 outputs degrees
+DEG_2_RAD = 0.01745329251; #trig functions require radians, BNO055 outputs degrees
 
 def talker():
-    global linearX
+    global linearX, xPos, yPos
     pub = rospy.Publisher('imu_topic', Imu, queue_size=10)
     rospy.init_node('imu_node', anonymous=True)
     rate = rospy.Rate(10) # 10hz
@@ -38,22 +41,22 @@ def talker():
        
         #msg.yaw=float("{0:.3f}".format(sensor.gyroscope[2])) #rad/s Rotation around Z-axis
         x_ori=float("{0:.3f}".format(sensor.euler[0])) #rad/s x orintation
-  	xPos = float(xPos + ACCEL_POS_TRANSITION * sensor.acceleration[0]) # x acc
-  	yPos = float(yPos + ACCEL_POS_TRANSITION * sensor.acceleration[1]) # y acc
+        xPos = float(xPos + ACCEL_POS_TRANSITION * sensor.acceleration[0]) # x acc
+        yPos = float(yPos + ACCEL_POS_TRANSITION * sensor.acceleration[1]) # y acc
         print("x ori" + str(x_ori))
-  	# velocity of sensor in the direction it's facing
-  	headingVel = ACCEL_VEL_TRANSITION * linearAccelData.acceleration.x / cos(DEG_2_RAD * sensor.euler[0]);
+        # velocity of sensor in the direction it's facing
+        headingVel = (ACCEL_VEL_TRANSITION * sensor.acceleration[0] / math.cos(DEG_2_RAD * sensor.euler[0])) * 100
         #aX= sensor.linear_acceleration[0]
         #time2= time.time()
         #dT= time2-time1
         #linearX += dT * aX #V = v_node + a*t
-        #msg.linearX=float("{0:.3f}".format(linearX))
-	msg.header.stamp = rospy.get_rostime()
+        msg.speed=float("{0:.3f}".format(headingVel))
+        msg.header.stamp = rospy.get_rostime()
         #output = (linearX, angularZ)
-	msg.yaw = x_ori
-	msg.pose_x = xPos
-	msg.pose_y = yPos
-	msg.speed = headingVel
+        msg.yaw = x_ori
+        msg.pose_x = xPos
+        msg.pose_y = yPos
+        msg.speed = headingVel
         rospy.loginfo(msg)
         pub.publish(msg)
         rate.sleep()
